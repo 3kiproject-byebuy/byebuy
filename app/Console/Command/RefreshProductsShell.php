9118df4d75047875cf0a786d184f3dbb72462e42
YujiHamada214 <yuji.hamada214@gmail.com>
2014-09-12 16:35:45 +0800

    140912_yuji
    140912_yuji

--- app/Console/Command/RefreshProductsShell.php
+++ app/Console/Command/RefreshProductsShell.php
@@ -4,43 +4,47 @@ class RefreshProductsShell extends AppShell {
     
 
 	//SellingList,WantedListモデルを使えるようにするために宣言	//定義済み関数
-	public $uses = array('SellingList','WantedList');
+	public $uses = array('Selling_list','Wanted_list');
 	
 
 
     public function main() {
 
-        // 0:募集中、1:掲載終了、2:取引完了 //
+        // status = 0:募集中、1:掲載終了、2:取引完了 //
 
-        
-        /* 掲載終了の場合（掲載終了日になった時点で商品削除） */  
 
-        //$delete_day_finish = date("Y-m-d H i s");
+        /* 掲載終了の場合（掲載終了日になった時点でdel_flgたてる） */ 
 
         //SellingList
-        //$conditions_selling_finish = array('Selling_list.deadline <=' => date("Y-m-d H i s"););
-        //$this->Selling_list->deleteAll($conditions_selling_finish, false);
+        $this->Selling_list->updateAll(
+                                array ('Selling_list.del_flg' => 1, 'Selling_list.status' => 1),
+                                array ('Selling_list.deadline <=' => date("Y-m-d H i s"), 'Selling_list.status' => 0)
+                                );
 
 
-        $this->テーブル名->updateAll(
-       array ( '更新するカラム' => 変更の値 ),
-       array ( 'Selling_list.deadline <=' => date("Y-m-d H i s")));
-
-        
 
 
-        /* 取引完了(status=2)の場合（取引完了５日後に商品削除） */ 
+
+
+
+        /* 取引完了(status=2)の場合（取引完了５日後にdel_flgたてる） */ 
 
         $delete_day = date("Y-m-d H i s", strtotime('-5 day'));
 
         //SellingList
-        $conditions_selling_trade_done = array('Selling_list.tradedate <=' => $delete_day, 'Selling_list.status' => 2);
-        $this->Selling_list->deleteAll($conditions_selling_trade_done , false);
+        $this->Selling_list->updateAll(
+                                array ('Selling_list.del_flg' => 1),
+                                array ('Selling_list.tradedate <=' => $delete_day, 'Selling_list.status' => 2)
+                                );
 
         //WantedList
-        $conditions_wanted_trade_done = array('Wanted_list.tradedate <=' => $delete_day, 'Wanted_list.status' => 2);
-        $this->WantedL_list->deleteAll($conditions_wanted_trade_done , false);
+        $this->Wanted_list->updateAll(
+                                array ('Wanted_list.del_flg' => 1),
+                                array ('Wanted_list.tradedate <=' => $delete_day,'Wanted_list.status' => 2)
+                                );
 
+
+        
         $this->out('Complete');
     }
 	
