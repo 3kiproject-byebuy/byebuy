bd6b5f113a7026f5c040deacb54e6f49f47bf3ec
YujiHamada214 <yuji.hamada214@gmail.com>
2014-09-15 20:06:46 +0800

    140915_2_yuji.dev
    app/Config/core.php修正
    app/Console/Exe_RefreshProductsShell.sh追加

---
+++ app/Console/Exe_RefreshProductsShell.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+/usr/local/php5.3/bin/php /home/users/0/lovepop.jp-geechscamp/web/byebuy/app/Console/cake.php RefreshProducts -app /home/users/0/lovepop.jp-geechscamp/web/byebuy/app
+
+ 
--- app/Console/RefreshProductsShell.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-php cake.php refresh_products
